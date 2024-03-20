7c0ba30dcb1e3776054de620a738e1c2669dfeb5
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-20 21:43:28 +0800

    Added bg music

--- src/data/scenes.ts
+++ src/data/scenes.ts
@@ -6,13 +6,13 @@ export const scenes: SceneType[] = [
     index: 0,
     sentence: "This is a simple Javascript test",
     media: "https://miro.medium.com/max/1024/1*OK8xc3Ic6EGYg2k6BeGabg.jpeg",
-    duration: 8
+    duration: 3
   },
   {
     index: 1,
     sentence: "yoyoyoy",
     media: "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_1280.jpg",
-    duration: 3
+    duration: 5
   },
   // {
   //   index: 1,
