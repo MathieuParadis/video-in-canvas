2588908d5320a99fb89e3e8aac950eca3f28e032
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-20 13:31:46 +0800

    Added text

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,5 +1,5 @@
 // REACT IMPORTS
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 // DATA IMPORTS
 import { scenes } from '../data/scenes';
@@ -7,6 +7,9 @@ import { scenes } from '../data/scenes';
 const Canvas = (): JSX.Element => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const canvasCtxRef = useRef<CanvasRenderingContext2D | null>(null);
+  const [currentScene, setCurrentScene] = useState(scenes[0])
+
+  console.log(setCurrentScene)
 
   useEffect(() => {
     if (canvasRef.current) {
@@ -19,12 +22,22 @@ const Canvas = (): JSX.Element => {
     }    
 
     const image = new Image();
-    image.src = scenes[0].media
+    image.src = currentScene.media
+
 
     image.onload = () => {
-      canvasCtxRef.current?.drawImage(image, 0, 0, canvasRef.current?.width ?? 300, canvasRef.current?.height ?? 168);
-    };
-  }, []);
+      if (canvasCtxRef.current) {
+        // Draw image
+        canvasCtxRef.current?.drawImage(image, 0, 0, canvasRef.current?.width ?? 300, canvasRef.current?.height ?? 168);
+
+        // Add text on top of the image
+        canvasCtxRef.current.font = '55px Arial';
+        canvasCtxRef.current.fillStyle = 'white';
+        canvasCtxRef.current.textAlign = 'right';
+        canvasCtxRef.current.fillText(currentScene.sentence, (canvasRef.current?.width ?? 300 / 2), 50);
+      };
+    }
+  }, [currentScene]);
 
   return (
     <div className='w-full h-full flex justify-center items-center'>
