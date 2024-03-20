635a52cc217d715074b807881ae661877940d78a
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-20 09:33:37 +0800

    Added data and types

---
+++ src/data/medias.ts
@@ -0,0 +1,17 @@
+// TYPES IMPORTS
+import { type MediaType } from '../types/mediaTypes'
+
+export const medias: MediaType[] = [
+  {
+    index: 0,
+    sentence: "This is a simple Javascript test",
+    media: "https://miro.medium.com/max/1024/1*OK8xc3Ic6EGYg2k6BeGabg.jpeg",
+    duration: 3
+  },
+  {
+    index: 0,
+    sentence: "This is a simple Javascript test",
+    media: "https://miro.medium.com/max/1024/1*OK8xc3Ic6EGYg2k6BeGabg.jpeg",
+    duration: 3
+  }
+]
