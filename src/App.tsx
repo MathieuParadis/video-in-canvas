fc7f8295aab212464dfc4d0438facac21500e56b
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-20 10:30:49 +0800

    created canvas component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 // REACT IMPORT
 import React from 'react';
 
-// ASSETS IMPORTS
-import logo from './logo.svg';
+// COMPONENTS IMPORTS
+import Canvas from './components/Canvas';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas />
     </div>
   );
 }
---
+++ src/components/Canvas.tsx
@@ -0,0 +1,12 @@
+// REACT IMPORT
+import React from 'react';
+
+const Canvas = (): JSX.Element => {
+  return (
+    <div>
+      canvas here
+    </div>
+  );
+}
+
+export default Canvas;
